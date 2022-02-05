a18d2262c9a169414777d97af6962a2ea3dd13d2
doyoque <abiyoga1335@gmail.com>
2022-02-06 03:12:21 +0700

    initialize footer component

--- doyoque-blog/blog/resources/js/pages/Index.vue
+++ doyoque-blog/blog/resources/js/pages/Index.vue
@@ -2,18 +2,21 @@
   <div class="grid grid-cols-6 gap-x-4">
     <greetingCompoent />
     <techStackComponent />
+    <footerComponent />
   </div>
 </template>
 
 <script>
 import greetingCompoent from '@components/GreetingComponent'
 import techStackComponent from '@components/TechStackComponent'
+import FooterComponent from '@components/FooterComponent'
 
 export default {
   name: 'Index',
   components: {
     greetingCompoent,
-    techStackComponent
+    techStackComponent,
+    FooterComponent
   }
 }
 </script>
