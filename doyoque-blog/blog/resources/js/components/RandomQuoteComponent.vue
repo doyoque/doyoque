52a283727fed9d1c20c290d7f6309f0996fe6d92
doyoque <abiyoga1335@gmail.com>
2022-02-23 23:01:32 +0700

    add more component for UI

---
+++ doyoque-blog/blog/resources/js/components/RandomQuoteComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div :class="['panel-card--blue-500 col-start-2 col-span-2']">
+    <h1>Today's quote: {{ quote }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RandomQuoteComponent',
+  data() {
+    return {
+      quote: 'the beginning'
+    }
+  }
+}
+</script>
---
+++ doyoque-blog/blog/resources/js/tests/components/RandomQuteComponent.spec.js
@@ -0,0 +1,12 @@
+import RandomQuote from '@components/RandomQuoteComponent'
+import { mount } from '@vue/test-utils'
+
+describe('Random Quote component', () => {
+  const wrapper = mount(RandomQuote)
+  const message = 'Today\'s quote:'
+
+  it ('Contain today quote text', () => {
+    expect(wrapper.text()).toContain(message)
+  })
+})
+
