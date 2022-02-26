e800ddff0ce81215ed28643d8489c3b89b93d98e
doyoque <abiyoga1335@gmail.com>
2022-02-26 14:46:12 +0700

    about pages

---
+++ doyoque-game/gim/pages/about.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>About doyoque-gim</h1>
+    <p>Doyoque-blog but game.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AboutPage',
+  head() {
+    return {
+      title: 'Doyoque-gim',
+      meta: [{
+        hid: 'description',
+        name: 'description',
+        content: 'Find out the philosophic thought about me playing some games.'
+      }]
+    }
+  }
+})
+</script>
