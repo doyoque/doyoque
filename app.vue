d17fb0d3b8970ec9504bcfec669f9737e626f3cf
Abiyoga Bayu Primadi <abiyogabayuprimadi@gmail.com>
2025-01-07 13:03:36 +0700

    feat: test meta tag

--- app.vue
+++ app.vue
@@ -3,9 +3,15 @@ const layout: string = 'default'
 
 useSeoMeta({
   title: 'Abiyoga Bayu Primadi - Doyoque',
-  ogTitle: 'Abiyoga Bayu Primadi - Doyoque',
   description: 'Fullstack Developer blogging or something.',
-  ogDescription: 'Fullstack Developer blogging or something.'
+  ogTitle: 'Abiyoga Bayu Primadi - Doyoque',
+  ogType: 'article',
+  ogUrl: 'https://doyoque.vercel.app/',
+  ogDescription: 'Fullstack Developer blogging or something.',
+  twitterCard: 'summary',
+  twitterSite: '@4E4F57',
+  twitterTitle: 'Abiyoga Bayu Primadi - Doyoque',
+  twitterDescription: 'Fullstack Developer blogging or something.'
 })
 </script>
 <template>
